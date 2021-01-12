17c6cee9a5854af5bba3a79a3a7691752a186a54
Zyansheep <zyansheep@gmail.com>
2021-01-12 14:11:11 -0500

    general key structure

---
+++ src/generators.rs
@@ -0,0 +1,13 @@
+
+use elliptic_curve::public_key:
+
+use crate::{SymmGenerator, AsymmGenerator};
+
+pub struct Secp256k1 {
+
+}
+impl AsymmGenerator for Secp256k1 {
+	fn generate() -> Vec<u8> {
+
+	}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,75 @@
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum AsymmType {
+	//Asymmetric Keys
+	Secp256k1,
+	NistP256,
+	NistP384,
+	RsaPkcsv1_5,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum SymmType {
+	AesGcmSiv,
+	AesGcm,
+	AesSiv,
+	CCM,
+	ChaCha20Poly1305,
+	Curve25519XSalsa20Poly1305,
+	MGM,
+	XSalsa20Poly1205,
+}
+
+
+impl AsymmType {
+	pub fn byte_translation(&self) -> u8 {
+		use AsymmType::*;
+		match *self {
+			Secp256k1 			=> 0,
+			NistP256			=> 1,
+			NistP384 			=> 2,
+			RsaPkcsv1_5 		=> 3,
+		}
+	}
+}
+impl SymmType {
+	pub fn byte_translation(&self) -> u8 {
+		use SymmType::*;
+		match *self {
+			AesGcmSiv 			=> 0,
+			AesGcm				=> 1,
+			AesSiv				=> 2,
+			CCM 				=> 3,
+			ChaCha20Poly1305	=> 4,
+			Curve25519XSalsa20Poly1305 => 5,
+			MGM 				=> 6,
+			XSalsa20Poly1205 	=> 7,
+		}
+	}
+}
+
+pub struct Multikey {
+	key: Vec<u8>
+}
+
+pub fn multikey(key_type: SymmType) {
+
+}
+pub fn multikeypair(key_type: AsymmType) {
+
+}
+
+
+pub trait AsymmGenerator {
+	fn generate() -> (Vec<u8>, Vec<u8>);
+}
+pub trait SymmGenerator {
+	fn generate() -> Vec<u8>;
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+	#[test]
+	fn it_works() {
+		assert_eq!(2 + 2, 4);
+	}
 }
